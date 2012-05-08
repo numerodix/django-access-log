7e3bab79e9f5936b97bc804f5ec3fca435b071a2
Martin Matusiak <numerodix@gmail.com>
2012-05-08 21:37:00 +0200

    add deps

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     url='https://numerodix@github.com/numerodix/django-access-log.git',
     download_url='https://numerodix@github.com/numerodix/django-access-log.git',
     license='GPL',
-    install_requires=join(dirname(__file__), 'requirements.txt').read(),
+    install_requires=open(join(dirname(__file__), 'requirements.txt')).read(),
 )
